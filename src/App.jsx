69782fd5a11e8cbfe6b110d200122a406052354c
ARCHITECTARIEL <arielsmailbox@gmail.com>
2026-07-20 15:57:03 -0400

    Route Admin to live operations dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,8 @@
-export {default} from './CupidV5.jsx';
+import CupidV5 from './CupidV5.jsx';
+import OpsDashboard from './OpsDashboard.jsx';
+import './ops-dashboard.css';
+
+export default function App(){
+ if(location.pathname==='/dashboard'||location.pathname==='/ops')return <OpsDashboard/>;
+ return <CupidV5/>;
+}
